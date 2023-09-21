4bb0b242a4fb60e06528ac4bdaf73341f1bb2966
Jisan-Hasan <jisan.jh5@gmail.com>
2023-09-22 01:46:15 +0600

    OfferedCourseClassSchedule UPDATE & DELETE API created

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.route.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.route.ts
@@ -1,10 +1,34 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { OfferedCourseClassScheduleController } from './offeredCourseClassSchedule.controller';
+import { OfferedCourseClassScheduleValidation } from './offeredCourseClassSchedule.validation';
 
 const router = express.Router();
 
-router.post('/', OfferedCourseClassScheduleController.insertIntoDB);
-
 router.get('/', OfferedCourseClassScheduleController.getAllFromDB);
+
+router.get('/:id', OfferedCourseClassScheduleController.getByIdFromDB);
+
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(OfferedCourseClassScheduleValidation.create),
+  OfferedCourseClassScheduleController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(OfferedCourseClassScheduleValidation.update),
+  OfferedCourseClassScheduleController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCourseClassScheduleController.deleteByIdFromDB
+);
 
 export const OfferedCourseClassScheduleRoutes = router;

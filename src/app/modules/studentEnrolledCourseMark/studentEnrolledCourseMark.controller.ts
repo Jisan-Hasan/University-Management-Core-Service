0cc11f4ba01ef1acf35df5ee61cd9743255ab8fd
Jisan-Hasan <jisan.jh5@gmail.com>
2023-09-26 22:38:47 +0600

    Student Enrolled Course Mark GET API created

--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.controller.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.controller.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
-import { StudentEnrolledCourseMarkService } from './studentEnrolledCourseMark.service';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { studentEnrolledCourseMarkFilterableFields } from './studentEnrolledCourseMark.constant';
+import { StudentEnrolledCourseMarkService } from './studentEnrolledCourseMark.service';
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, studentEnrolledCourseMarkFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await StudentEnrolledCourseMarkService.getAllFromDB(
+    filters,
+    options
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student course marks fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
 
 const updateStudentMarks = catchAsync(async (req: Request, res: Response) => {
   const result = await StudentEnrolledCourseMarkService.updateStudentMarks(
@@ -17,5 +35,6 @@ const updateStudentMarks = catchAsync(async (req: Request, res: Response) => {
 });
 
 export const StudentEnrolledCourseMarkController = {
+  getAllFromDB,
   updateStudentMarks,
 };
